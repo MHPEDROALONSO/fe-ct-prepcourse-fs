6545dc3b572a4dd0bdd6106c0558d20f1c25a2cb
peterlife <peterlife23@gmail.com>
2023-08-15 12:07:37 +0100

    Homework_04DONE

---
+++ M04 JavaScript Bucles/OPERDORES LOGICOS.js
@@ -0,0 +1,31 @@
+//function mayorYMenorYPar (num){
+  //  if (num >5 && num <10 &&num %2===0)console.log(true);
+  //  else console.log(false);
+//}
+//mayorYMenorYPar(7);
+//mayorYMenorYPar(8)
+
+//function operador(str){
+  //  if (str==='Henry'|| str.length<2) console.log(true);
+  //  else console.log(false);
+//}
+//operador('henry')
+//operador('javas0')
+//operador('H')
+
+//function negacion(permiso){
+ //   if (permiso)console.log('tiene permiso');
+//}
+//negacion(false)
+//negacion(true)
+
+suma=0
+//for (let e=0;e<15;e++){
+//    suma=suma+e;
+//    console.log('variable de iteracion: ',e);
+//}
+//console.log('variable de suma: ',suma);
+while(suma<4){
+    suma=suma+1;
+    console.log(suma);
+}
